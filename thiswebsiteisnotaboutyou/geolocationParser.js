a6b718cce5681c2680b941cb5aeb92b88f4b5f33
yanton <ygoldobin@gmail.com>
2019-12-13 02:49:40 +0100

    first commit, app scafolding and geoLocationParser

---
+++ thiswebsiteisnotaboutyou/geolocationParser.js
@@ -0,0 +1,41 @@
+const geoip = require("geoip-lite");
+
+const isLocalDevIp = ip => ip === "127.0.0.1" || ip === "::1";
+
+function getIpIfAvailable(req) {
+  // if request routed via proxy, req.ip contains x-forwarded-for original IP
+  let ip = req.ip || req.connection.remoteAddress || false;
+
+  if (isLocalDevIp(ip)) {
+    return false;
+  }
+  // IPV6 address can look like ::ffff:192.168.0.1'
+  if (ip && ip.includes("::ffff:")) {
+    ip = ip.split(":").reverse()[0];
+  }
+
+  return ip;
+}
+
+const geolocationParser = function(req, res, next) {
+  let geoIpData;
+  let parsedIp;
+
+  if (req.query.ip) {
+    parsedIp = req.query.ip;
+  } else {
+    parsedIp = getIpIfAvailable(req);
+  }
+
+  // TODO: probably remove this
+  req.parsedIp = parsedIp;
+
+  if (parsedIp) {
+    geoIpData = geoip.lookup(ip);
+    req.geoIpData = geoIpData;
+  }
+
+  next();
+};
+
+module.exports = geolocationParser;
---
+++ thiswebsiteisnotaboutyou/server.js
@@ -0,0 +1,48 @@
+// This file doesn't go through babel or webpack transformation.
+// Make sure the syntax and sources this file requires are compatible with the current node version you are running
+// See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
+// const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+// Express middlewares
+const morgan = require("morgan");
+const geolocationParser = require("./geolocationParser");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const LOGGER_FORMAT = dev
+  ? "dev"
+  : ":method :url :status :res[content-length] - :response-time ms";
+
+app.prepare().then(() => {
+  const server = express();
+
+  // console.warn("request", { req });
+  // console.warn("response", { res });
+  server.use(cookieParser());
+  server.use(morgan(LOGGER_FORMAT));
+  server.use(geolocationParser);
+
+  // Be sure to pass `true` as the second argument to `url.parse`.
+  // This tells it to parse the query portion of the URL.
+  // const { pathname, query } = parsedUrl
+
+  server.get("*", (req, res) => {
+    console.warn("req.ip", req.ip);
+    console.warn("req.cookies", req.cookies);
+    console.warn("req.query", req.query);
+    console.warn("req.parsedIp", req.parsedIp);
+
+    return handle(req, res);
+  });
+
+  server.listen(3000, err => {
+    if (err) throw err;
+    console.log("> Ready on http://localhost:3000");
+  });
+});
