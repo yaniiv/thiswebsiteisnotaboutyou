06d0803c29dc66f90fcd8f8f1be8f4a38c9fffb3
yaniv goldobin <ygoldobin@gmail.com>
2020-04-13 23:43:51 -0700

    ip

--- components/Boxes.js
+++ components/Boxes.js
@@ -31,7 +31,7 @@ const Boxes = ({ boxSize, colors, setSelectedIndex }) => {
                   0px 24px 38px 3px rgba(0, 0, 0, 0.14),
                   0px 9px 46px 8px rgba(0, 0, 0, 0.12);
                 color: rgba(0, 0, 0, 0.87);
-                border: 2px solid white;
+                border: 3px solid #036cdb;
                 /* background-color: black; */
                 /* margin-top: -24px; */
                 /* margin-left: -24px; */
