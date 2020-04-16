d8afb342a44335746ffb81c852a8ce2570401aab
yaniv goldobin <ygoldobin@gmail.com>
2020-04-16 14:41:28 -0700

    remove unused animation snippets

--- pages/index.js
+++ pages/index.js
@@ -11,54 +11,6 @@ import BodyText from "../components/BodyText";
 import Nav from "../components/NavCons";
 import Contribute from "../components/Contribute";
 
-const basicStyles = css`
-  background-color: white;
-  color: cornflowerblue;
-  border: 1px solid lightgreen;
-  border-right: none;
-  border-bottom: none;
-  box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
-  transition: all 0.1s linear;
-  margin: 3rem 0;
-  padding: 1rem 0.5rem;
-`;
-const hoverStyles = css`
-  &:hover {
-    color: white;
-    background-color: lightgray;
-    border-color: aqua;
-    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
-  }
-`;
-const bounce = keyframes`
-  from {
-    transform: scale(1.01);
-  }
-  to {
-    transform: scale(0.99);
-  }
-`;
-
-const Basic = styled.div`
-  ${basicStyles};
-`;
-
-const Combined = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-`;
-const Animated = styled.div`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-  animation: ${(props) => props.animation} 0.2s infinite ease-in-out alternate;
-`;
-
 function isBrowser() {
   return typeof window !== "undefined";
 }
@@ -168,6 +120,7 @@ const Landing = ({ data, parsedIp, geoIpData }) => {
             />
             {isContributeFormActive && (
               <Contribute
+                parsedIp={parsedIp}
                 setIsContributeFormActive={setIsContributeFormActive}
                 isContributeFormActive={isContributeFormActive}
               />
