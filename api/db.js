5a815dc997bbc2de6aff6fda600a78f129645305
yaniv goldobin <ygoldobin@gmail.com>
2020-04-17 19:23:43 -0700

    remove code

--- api/db.js
+++ api/db.js
@@ -1,8 +1,5 @@
 const mongoose = require("mongoose");
-// const getConfig = require("next/config")
-// const config = require("../next.config.js");
 const contributionSchema = require("./schema");
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 const DB_URI = process.env.DB_URI;
 console.warn("DB_URI", DB_URI);
